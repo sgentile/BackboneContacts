f10dead23fa1e688024722ef5e33553c9b60d834
sgentile <steven.gentile@gmail.com>
2011-12-16 09:41:34 -0500

    updated the 'AddContact' to using ModelBinding

--- Contacts/Scripts/app2.js
+++ Contacts/Scripts/app2.js
@@ -1,5 +1,6 @@
 ﻿/// <reference path="backbone.js" />
 /// <reference path="backbone.marionette.js" />
+/// <reference path="backbone.modelbinding.js" />
 
 /******  APP ******/
 
@@ -54,108 +55,108 @@ ViewSwitcherApp.addInitializer(function () {
 
 /******  CONTACTS ******/
 ViewSwitcherApp.Contacts = (function (ViewSwitcherApp, Backbone) {
-	var Contacts = {};
-	Contacts.ContactModel = Backbone.Model.extend({
-		defaults: {
-			id: null,
-			firstname: "",
-			lastname: ""
-		},
-		url: function (type) {
-			if (type == "DELETE")
-			    return "Contact/delete/" + this.get('id');
-			return this.isNew() ? "Contact/create" : "Contact/update";
-		}
-	});
-	Contacts.ContactModels = Backbone.Collection.extend({
-		model: Contacts.ContactModel,
-		url: "Contact/list"
-		//url: "http://localhost/Contacts/contact/list"
-	});
+    var Contacts = {};
+    Contacts.ContactModel = Backbone.Model.extend({
+        defaults: {
+            id: null,
+            firstname: "",
+            lastname: ""
+        },
+        url: function (type) {
+            if (type == "DELETE")
+                return "Contact/delete/" + this.get('id');
+            return this.isNew() ? "Contact/create" : "Contact/update";
+        }
+    });
+    Contacts.ContactModels = Backbone.Collection.extend({
+        model: Contacts.ContactModel,
+        url: "Contact/list"
+        //url: "http://localhost/Contacts/contact/list"
+    });
 
-	Contacts.contacts = new Contacts.ContactModels();
+    Contacts.contacts = new Contacts.ContactModels();
 
-	Contacts.ContactView = Backbone.View.extend({
-		tagName: 'li',
-		model: Contacts.ContactModel,
-		initialize: function () {
-			this.template = $("#contact-template");
-			_.bindAll(this, "render");
-		},
-		events: {
-			"click span.remove-contact": "remove"
-		},
-		render: function () {
-			//render the jQuery template
-			var content = this.template.tmpl(this.model.toJSON());
-			//take the rendered HTML and pop it into the DOM
-			$(this.el).html(content);
+    Contacts.ContactView = Backbone.View.extend({
+        tagName: 'li',
+        model: Contacts.ContactModel,
+        initialize: function () {
+            this.template = $("#contact-template");
+            _.bindAll(this, "render");
+        },
+        events: {
+            "click span.remove-contact": "remove"
+        },
+        render: function () {
+            //render the jQuery template
+            var content = this.template.tmpl(this.model.toJSON());
+            //take the rendered HTML and pop it into the DOM
+            $(this.el).html(content);
 
-			return this;
-		},
-		remove: function () {
-			this.model.destroy();
-		}
-	});
+            return this;
+        },
+        remove: function () {
+            this.model.destroy();
+        }
+    });
 
-	Contacts.ContactsListView = Backbone.View.extend({
-		//el: "#contacts-list",
-		//model: Contact,
-		initialize: function () {
-			_.bindAll(this, "render");
-			this.collection.bind("change", this.render);
-			this.collection.bind("add", this.render);
-			this.collection.bind("fetch", this.render);
-			this.collection.bind('remove', this.render);
+    Contacts.ContactsListView = Backbone.View.extend({
+        //el: "#contacts-list",
+        //model: Contact,
+        initialize: function () {
+            _.bindAll(this, "render");
+            this.collection.bind("change", this.render);
+            this.collection.bind("add", this.render);
+            this.collection.bind("fetch", this.render);
+            this.collection.bind('remove', this.render);
 
-			Contacts.contacts.fetch({ add: true });
-		},
-		render: function () {
-			//clear out the existing list to avoid "append" duplication
-			$(this.el).empty();
-			//use an array here rather than firehosing the DOM
-			//perf is a bit better
-			var els = [];
-			//loop the collection...
-			this.collection.models.forEach(function (contact) {
-				//rendering a view for each model in the collection
-				var view = new Contacts.ContactView({ model: contact });
-				//adding it to our array
-				els.push(view.render().el);
-			});
-			//push that array into this View's "el"
-			$(this.el).append(els);
-			return this;
-		}
-	});
+            Contacts.contacts.fetch({ add: true });
+        },
+        render: function () {
+            //clear out the existing list to avoid "append" duplication
+            $(this.el).empty();
+            //use an array here rather than firehosing the DOM
+            //perf is a bit better
+            var els = [];
+            //loop the collection...
+            this.collection.models.forEach(function (contact) {
+                //rendering a view for each model in the collection
+                var view = new Contacts.ContactView({ model: contact });
+                //adding it to our array
+                els.push(view.render().el);
+            });
+            //push that array into this View's "el"
+            $(this.el).append(els);
+            return this;
+        }
+    });
 
-	Contacts.AddContactView = Backbone.View.extend({
-		initialize: function () {
-			this.template = $("#add-contact-template");
-		},
-		render: function () {
-			var content = this.template.tmpl();
-			$(this.el).html(content);
-
-			return this;
-		},
-		events: {
-			//"click input[type=button]": "addContact"
-			"click #addContact_button": "addContact"
-		},
-		addContact: function (event) {
-			var model = new Contacts.ContactModel({ firstname: $("#firstname").val(), lastname: $("#lastname").val() });
-			Contacts.contacts.create(model);
-			//this.$("firstname").val("");
-			//this.$("lastname").val("");
-		}
-	});
-	Contacts.show = function () {
-		ViewSwitcherApp.mainRegion.show(new Contacts.AddContactView());
-		ViewSwitcherApp.contactsRegion.show(new Contacts.ContactsListView({ collection: Contacts.contacts }));
-		ViewSwitcherApp.showRoute("");
-	};
-	return Contacts;
+    Contacts.AddContactView = Backbone.View.extend({
+        initialize: function () {
+            this.template = $("#add-contact-template");
+            this.model = new Contacts.ContactModel();
+        },
+        render: function () {
+            var content = this.template.tmpl();
+            $(this.el).html(content);
+            Backbone.ModelBinding.bind(this);
+            return this;
+        },
+        events: {
+            "click #addContact_button": "addContact"
+        },
+        addContact: function (event) {
+            Contacts.contacts.create(this.model);
+            Backbone.ModelBinding.unbind(this);
+            ViewSwitcherApp.mainRegion.show(new Contacts.AddContactView());
+        }
+    });
+    
+    Contacts.show = function () {
+        ViewSwitcherApp.mainRegion.show(new Contacts.AddContactView());
+        ViewSwitcherApp.contactsRegion.show(new Contacts.ContactsListView({ collection: Contacts.contacts }));
+        ViewSwitcherApp.showRoute("");
+    };
+    return Contacts;
 })(ViewSwitcherApp, Backbone);
 
 /******  VIEWONE ******/
